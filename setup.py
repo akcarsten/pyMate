7d4857813a41699ef54130bb6d81c291f1b3f8b7
akcarsten <carsten.a.klein@gmail.com>
2024-03-31 22:38:02 +0200

    setup configured

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pyMate',
+    version='1.0.0',
+    packages=[''],
+    url='https://github.com/akcarsten/pyMate',
+    license='MIT',
+    author='Carsten Klein',
+    author_email='https://www.linkedin.com/in/carsten-klein/',
+    description='pyMate is a Python package that provides basic functionalities to process functional magnetic '
+                'resonance imaging (fMRI) and electro-physiological (e-phys) data.'
+)
